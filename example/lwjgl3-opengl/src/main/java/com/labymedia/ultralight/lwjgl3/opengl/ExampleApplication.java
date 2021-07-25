52171fe608dc418fb16f2df0a562eef8953fe7c8
Janrupf <business.janrupf@gmail.com>
2021-07-25 21:00:55 +0200

    Change example back to example.html

--- example/lwjgl3-opengl/src/main/java/com/labymedia/ultralight/lwjgl3/opengl/ExampleApplication.java
+++ example/lwjgl3-opengl/src/main/java/com/labymedia/ultralight/lwjgl3/opengl/ExampleApplication.java
@@ -227,7 +227,7 @@ public class ExampleApplication {
         glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
 
         // Load a local test file
-        webController.loadURL("https://youtube.com");
+        webController.loadURL("file:///example.html");
 
         double lastTime = glfwGetTime();
         int frameCount = 0;
